9e98356f08254aca63084de950b36b2d40340a15
vitosabalic <vito.sabalic@fer.hr>
2020-10-29 18:32:41 +0100

    Removed folders local and cyfer-project-backend

--- local/src/main/java/cyfer/dao/DogRepository.java
+++
@@ -1,7 +0,0 @@
-package cyfer.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import cyfer.domain.Dog;
-
-public interface DogRepository extends CrudRepository<Dog, Integer> {}
