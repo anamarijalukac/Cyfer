09fe9084f8b503f4898e12aa62afb31e3926ac9f
Anamarija <al51487@fer.hr>
2020-12-22 17:09:22 +0100

    added functionalities

--- backend/src/main/java/cyfer/CyferApplication.java
+++ backend/src/main/java/cyfer/CyferApplication.java
@@ -9,24 +9,24 @@ public class CyferApplication {
 	//ne radi nam shelter.getbyid
 
 	/*
-	 * UC1 - Pregled profila udruga i pasa iz te udruge
+	 * UC1 - Pregled profila udruga i pasa iz te udruge-boki
 	 * UC2 - Pregled liste profila svih pasa-*
 	 * UC3 - Pregled statistike svih pasa-*
-	 * UC4 - Pregled rang-liste svih setaca
+	 * UC4 - Pregled rang-liste svih setaca-dave
 	 * UC5 - Registracija korisnika (gradanina ili udruge) u sustav-*
 	 * UC6 - Prijava korisnika (gradanina ili udruge) u sustav-*
 	 * UC7 - Pregled osobnih podataka korisnika (udruge ili gradanina)-*
 	 * UC8 - Promjena osobnih podataka korisnika-*
 	 * UC9 - Brisanje korisnickog racuna (udruge ili gradanina)-*
-	 * UC10 - Dodavanje novog profila psa u listu prijavljene udruge
-	 * UC11 - Uredivanje profila psa neke udruge
-	 * UC12 - Brisanje profila psa iz liste neke udruge
+	 * UC10 - Dodavanje novog profila psa u listu prijavljene udruge-sven
+	 * UC11 - Uredivanje profila psa neke udruge-boki
+	 * UC12 - Brisanje profila psa iz liste neke udruge-dave
 	 * UC13 - Rezervacija termina setnje-*
-	 * UC14 - Pregled vlastitih statistika setnji
-	 * UC15 - Oznacavanje statistike setnji kao javne 
-	 * UC16 - Pregled i skidanje (eng.download) kalendara registriranog gradanina
-	 * UC17 - Pregled korisnika
-	 * UC18 - Brisanje korisnika
+	 * UC14 - Pregled vlastitih statistika setnji-sven
+	 * UC15 - Oznacavanje statistike setnji kao javne -sven
+	 * UC16 - Pregled i skidanje (eng.download) kalendara registriranog gradanina-dave
+	 * UC17 - Pregled korisnika-zaba
+	 * UC18 - Brisanje korisnika-zaba
 	 */
 
 	public static void main(String[] args) {
