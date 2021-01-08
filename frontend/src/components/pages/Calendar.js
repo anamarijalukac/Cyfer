58fde96327f08e1b89003a87756607bd9d31c7e7
petra.presecki <pp51719@fer.hr>
2021-01-08 01:11:56 +0100

    Dodan download pdf-a

--- frontend/src/components/pages/Calendar.js
+++ frontend/src/components/pages/Calendar.js
@@ -1,25 +1,41 @@
-import React, { Component } from 'react'  
-import FullCalendar from "@fullcalendar/react";  
-import dayGridPlugin from "@fullcalendar/daygrid";  
-import timeGridPlugin from "@fullcalendar/timegrid";  
-  
+import React, { Component } from 'react'
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import ReactToPdf from "react-to-pdf";
+import '../../components/pages/profile.css';
+
 function Calendar() {
-    const data = JSON.parse(localStorage.getItem("reservation"));
-    console.log(data);
-    const events = [{ title: "Today", date: new Date() }];  
-        return (   
-                 <FullCalendar  
-              defaultView="dayGridMonth"  
-             header={{  
-            left: "prev,next",  
-            center: "title",  
-           right: "dayGridMonth,timeGridWeek,timeGridDay"  
-        }}  
-        plugins={[dayGridPlugin, timeGridPlugin]}  
-        events={events}  
-      />  
-        )
-  
-}  
-  
+  const data = JSON.parse(localStorage.getItem("reservation"));
+  const options = {
+    orientation: 'landscape',
+}
+  console.log(data);
+  const events = [{ title: "Today", date: new Date() }];
+  const ref = React.createRef();
+  return (
+    <div className="cal" ref={ref} >
+      <FullCalendar
+        defaultView="dayGridMonth"
+        header={{
+          left: "prev,next",
+          center: "title",
+          right: "dayGridMonth,timeGridWeek,timeGridDay"
+        }}
+        plugins={[dayGridPlugin, timeGridPlugin]}
+        events={events}
+      />
+
+      <div>
+        <ReactToPdf targetRef={ref} options={options} filename="raspored_setnje.pdf" x={.5} y={.7} scale={0.8}>
+          {({ toPdf }) => (
+            <button onClick={toPdf}>Skini u pdf-u</button>
+          )}
+        </ReactToPdf>
+      </div>
+    </div>
+  )
+
+}
+
 export default Calendar
